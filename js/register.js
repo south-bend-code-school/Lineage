ac0163bf9827b5e27fd86b1111e00bade8cd9725
asejdina <asejdina@gmail.com>
2017-02-12 23:00:56 -0500

    added js for registration. working out error around uploading image

--- js/register.js
+++ js/register.js
@@ -12,10 +12,38 @@
   function init () {
     firebase.initializeApp(config);
     $('select').material_select();
+    $('#submit').click(register);
   }
 
   function register(){
-    
+    var fName = $('#first_name').val();
+    var lName = $('#last_name').val();
+    var city = $('#city').val();
+    var state = $('#state').val();
+    var email = $('#email').val();
+    var password = $('#password').val();
+
+    var userData = {
+      fName : fName,
+      lName : lName,
+      city : city,
+      state : state,
+    };
+
+    var updates = {};
+    console.log('here')
+    firebase.auth().createUserWithEmailAndPassword(email,password)
+      .then(function(user){
+        firebase.storage().ref().child("images/users/"+user.uid).put($(".photo")[0].files[0])
+          .then(function(snapshot){
+            updates['/User/' + user.uid] = userData;
+            return firebase.database().ref().update(updates)
+              .then(function(){
+                window.location.replace('./profile.html?name='+user.uid);
+              });
+          });
+      });
+
   }
 
 })();
