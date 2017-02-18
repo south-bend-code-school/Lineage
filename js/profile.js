92f5cb5f171a712850121f40e6a3b2e1621ea2ee
Chris Frederick <cfreder2@nd.edu>
2017-02-18 01:31:45 -0500

    Updated home page, added support for video stories on timeline, rebuilt navbar (still have to fix color)

--- js/profile.js
+++ js/profile.js
@@ -31,7 +31,7 @@
   function loadUserImage() {
 
     firebase.storage().ref().child('images/users/' + userKey).getDownloadURL().then(function(url) {
-     $('.card-image').append('<img src='+url+'>')
+     $('.clip-circle').append('<img src='+url+' style="height:56px;width:56px;">')
    }).catch(function(error) {
      console.log(error);
    });
@@ -51,19 +51,41 @@
 
 
 
-          var html = "<div class='card large'>" +
+          var html
+          if(user.video_story) {
+            html = "<div class='card medium' style='height:480px; width:640px;'>" +
+                         "<div class='card-image waves-effect waves-block waves-light'>" +
+                           "<img class='activator' style='background-posting:center top; width=100%; height:auto' src='"+user.media_url+"'>" +
+                         "</div>" +
+                         "<div class='card-content center'>"+
+                           "<span class='card-title activator grey-text text-darken-4'>" + user.title +
+                           "<p><em class='card-title activator'>Story Year: " + user.story_year + "</em></p>" +
+                           "<div class='btn activator teal lighten-3 center'>Watch Story</div>" +
+                         "</div>" +
+                       "<div class='card-reveal'>" +
+                         "<span class='card-title activator grey-text text-darken-4'>" + user.title + "<i class='material-icons right'>close</i></span>" +
+                         "<iframe width='560' height='315' src='" + user.video_url + "?rel=0&amp;showinfo=0' frameborder='0' allowfullscreen></iframe>" +
+                         "<p>" + user.desc + "</p>" +
+                       "</div>" +
+                     "</div>"
+
+
+          } else {
+           html = "<div class='card medium' style='height:480px; width:640px;'>" +
                         "<div class='card-image waves-effect waves-block waves-light'>" +
                           "<img class='activator' style='background-posting:center top; width=100%; height:auto' src='"+user.media_url+"'>" +
                         "</div>" +
-                     "<div class='card-content'>"+
-                        "<span class='card-title activator grey-text text-darken-4'>" + user.title + "<i class='material-icons right'>more_vert</i></span>" +
-                        "<p><a href='#'>This is a link</a></p>" +
-                    "</div>" +
-                    "<div class='card-reveal'>" +
-                      "<span class='card-title activator grey-text text-darken-4'>" + user.title + "<i class='material-icons right'>close</i></span>" +
-                      "<p>" + user.desc + "</p>" +
-                    "</div>" +
+                        "<div class='card-content center'>"+
+                          "<span class='card-title activator grey-text text-darken-4'>" + user.title +
+                          "<p><em class='card-title activator'>Story Year: " + user.story_year + "</em></p>" +
+                          "<div class='btn activator teal lighten-3 center'>Read Story</div>" +
+                        "</div>" +
+                      "<div class='card-reveal'>" +
+                        "<span class='card-title activator grey-text text-darken-4'>" + user.title + "<i class='material-icons right'>close</i></span>" +
+                        "<p>" + user.desc + "</p>" +
+                      "</div>" +
                     "</div>"
+          }
           $('#leaves').append(html);
 
           // <div class="card">
